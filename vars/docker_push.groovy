cc2f348d56d7787763f8c54fa8580cb20e50d1c9
Amaan Ul Haq Siddiqui <amaanulhaq55@gmail.com>
2024-11-02 12:27:14 +0500

    docker fix

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,15 +1,15 @@
-def call(String project, String imageTag) {
+def call(String project, String imageTag, String dockerUser) {
     withCredentials([usernamePassword(credentialsId: "HubDocker", 
                                      passwordVariable: "env.HubDockerPass", 
                                      usernameVariable: "env.HubDockerUser")]) {
         // Log in to Docker Hub
-        sh "echo ${env.HubDockerPass} | docker login -u ${env.HubDockerUser} --password-stdin"
+        sh "echo ${env.HubDockerPass} | docker login -u ${dockerUser} --password-stdin"
         
         // Tag the image with Docker Hub repository
-        sh "docker image tag ${project}:${imageTag} ${env.HubDockerUser}/${project}:${imageTag}"
+        sh "docker image tag ${project}:${imageTag} ${dockerUser}/${project}:${imageTag}"
         
         // Push the tagged image to Docker Hub
-        sh "docker push ${env.HubDockerUser}/${project}:${imageTag}"
+        sh "docker push ${dockerUser}/${project}:${imageTag}"
         
         // Log out after pushing
         sh "docker logout"
