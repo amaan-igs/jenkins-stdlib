080bb2a7084e5d7da88f44242c885f9121885d5e
Amaan Ul Haq Siddiqui <amaanulhaq55@gmail.com>
2024-11-02 11:54:14 +0500

    docker +

--- docker_build.groovy
+++ docker_build.groovy
@@ -1,9 +1,5 @@
 def call(String projectName, String imageTag, String dockerHubUser) {
-
-    if (!projectName || !imageTag || !dockerHubUser) {
-        error("Required parameters are missing: Please provide ProjectName, ImageTag, and DockerHubUser.")
-    }
+    def imageName = dockerHubUser ? "${dockerHubUser}/${projectName}:${imageTag}" : "${projectName}:${imageTag}"
     
-    // Docker build command
-    sh "docker build -t ${dockerHubUser}/${projectName}:${imageTag} ."
+    sh "docker build -t ${imageName} ."
 }
