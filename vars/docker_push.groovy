8ad5458f02a72ac80cc35236db0d419fe47e399d
Amaan Ul Haq Siddiqui <amaanulhaq55@gmail.com>
2024-11-02 12:12:01 +0500

    docker +

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,7 +1,12 @@
 def call(String project, String imageTag, String dockerHubUser) {
     withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
+        // Print out variables for debugging
+        echo "Docker Hub User: ${dockerHubUser}"
+        echo "Docker Hub Project: ${project}"
+        echo "Image Tag: ${imageTag}"
+
         // Log in to Docker Hub
-        sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
+        sh "echo ${dockerHubPass} | docker login -u ${dockerHubUser} --password-stdin"
 
         // Tag the image with Docker Hub repo
         sh "docker tag ${project}:${imageTag} ${dockerHubUser}/${project}:${imageTag}"
